e35ff56d1a0f8bb9ca1d33dddb10ec7ee7e79bd7
yongseok1991 <dobest09998@gmail.com>
2025-04-04 14:37:16 +0900

    sss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
-import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap 스타일 추가
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename="/game.github.io">
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
     );
